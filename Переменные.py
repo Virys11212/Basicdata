681fcd0eff63de02b71bbff31b7c8852c7c5bac3
virys1121 <viryc1121@yandex.ru>
2024-08-29 23:54:49 +0300

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,15 @@
+#Number_of_DZ
+NODZ = 12
+#Number_of_hours_spent
+NOHS = 1.5
+#Course_name
+CN= 'Python'
+#Time_for_one_task
+TFOT= 0.125
+
+print('Курс:',CN)
+print('всего задач:',NODZ)
+print('затрачено часов:',NOHS)
+print('среднее время выполнения:',TFOT)
+
+print('Курс:',CN,';','всего задач:',NODZ,';','затрачено часов:',NOHS,';','среднее время выполнения:',TFOT,'.' )
