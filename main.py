cfc8f30d966298f392c18f24203fe3617bbb0173
Virys11212 <viryc1121@yandex.ru>
2024-08-29 20:26:07 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+print('_____________________________________')
+# 2nd program
+print('%1')
+print(9 ** 0.5 * 5)
+
+print('_____________________________________')
+# 2nd program
+print('%2')
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+print('_____________________________________')
+# 3rd program
+print("%3")
+print(2 * 2 + 2, 2 * (2 + 2))
+print(2 * 2 + 2 == 2 * (2 + 2))
+
+print('_____________________________________')
+# 4th program
+print('%4')
+num = '123.456'
+index = num.find('.')
+result = num[index + 1]
+print(result)
+
+print('_____________________________________')
+s = '123.456'
+num = float(s)
+num *= 10
+integer_part = int(num)
+fractional_part = num % 10
+result = integer_part + fractional_part
+print(result)
+print('_____________________________________')
