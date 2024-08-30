2fcbdddccfb2e220cf30be64b830a54481b334c0
virys1121 <viryc1121@yandex.ru>
2024-08-31 01:45:54 +0300

    Update Динамическая типизация.py

--- Динамическая типизация.py
+++ Динамическая типизация.py
@@ -1,17 +1,8 @@
-#Number_of_DZ
-NODZ = 12
-#Number_of_hours_spent
-NOHS = 1.5
-#Course_name
-CN= 'Python'
-time = 12.0
-speed = 1.5
-#Time_for_one_task
-TFOT = time / speed
-
-print('Курс:',CN)
-print('всего задач:',NODZ)
-print('затрачено часов:',NOHS)
-print('среднее время выполнения:',TFOT)
-
-print('Курс:',CN,';','всего задач:',NODZ,';','затрачено часов:',NOHS,';','среднее время выполнения:',TFOT,'.' )
+name = 'Lev'
+print(name)
+aqe = 19
+print(aqe)
+aqe = 20
+print(aqe)
+is_student = True
+print(is_student)
