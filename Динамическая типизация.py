eafc210d9da321cbf007850926eaebf9b36d98b3
virys1121 <viryc1121@yandex.ru>
2024-08-31 01:19:06 +0300

    Add files via upload

---
+++ Динамическая типизация.py
@@ -0,0 +1,17 @@
+#Number_of_DZ
+NODZ = 12
+#Number_of_hours_spent
+NOHS = 1.5
+#Course_name
+CN= 'Python'
+time = 12.0
+speed = 1.5
+#Time_for_one_task
+TFOT = time / speed
+
+print('Курс:',CN)
+print('всего задач:',NODZ)
+print('затрачено часов:',NOHS)
+print('среднее время выполнения:',TFOT)
+
+print('Курс:',CN,';','всего задач:',NODZ,';','затрачено часов:',NOHS,';','среднее время выполнения:',TFOT,'.' )
